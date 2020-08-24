3dd192e449846f7e600ee3fb2a6032f15abc2a16
Srinivasan R <babbloo85@gmail.com>
2020-08-24 10:58:36 -0700

    minor

--- scripts/notebooks/trigger_ilc_spt.py
+++ scripts/notebooks/trigger_ilc_spt.py
@@ -24,7 +24,9 @@ if (1):
     #exparr = ['spt3g', 'spt4_C1', 'spt4_C2', 'spt4_C3', 'spt4_C4', 'spt4_C5']
     exparr = ['spt3g', 'spt4_C3']
     #exparr = ['spt3g', 'sptpolplusultradeepplus3gplusherschel']#, 'spt4_C3'] #CIB at spt4 bands are not computed for websky
-    exparr = ['sptpolplusultradeepplus3gplusherschel']#, 'spt4_C3'] #CIB at spt4 bands are not computed for websky
+    #exparr = ['sptpolplusultradeepplus3gplusherschel']#, 'spt4_C3'] #CIB at spt4 bands are not computed for websky
+    exparr = ['spt4_C3'] #CIB at spt4 bands are not computed for websky
+    #exparr = ['spt4_C3plusplanckHF'] #CIB at spt4 bands are not computed for websky
 
     #exparr = ['sptpolplusultradeepplus3gplusherschel_v2', 'sptpolplusultradeepplus3gplusherschel_v3', 'sptpolplusultradeepplus3gplusherschel_v4'] #CIB at spt4 bands are not computed for websky
 
@@ -43,13 +45,18 @@ if (1):
     #null_comp_arr = ['misc_cib_tcib20_beta1.5 misc_cib_tcib20_beta2.5 radio']
     null_comp_arr = [ null_comp_arr[0], 'y %s' %(null_comp_arr[0]) ]
 
+
+    if exparr[0].find('spt4')>-1:
+        null_comp_arr = ['y misc_cib_tcib20_beta1.5', 'y misc_cib_tcib25_beta1.5']
+
+
     #null_comp_arr = ['y', 'radio']
     split_cross = 0 ###1
 
     #null_comp_arr = ['cibpo cibclus y']
 
-    use_websky_cib = 0 ## ###0 ##1
-    use_sptspire_for_hfbands = 1 ###1 ##1 ##0
+    use_websky_cib = 1 ## ###0 ##1
+    use_sptspire_for_hfbands = 0 ###1 ##1 ##0
     use_mdpl2_cib = 0
     final_comp = 'cmb'
 
