e8ce0ec00fd832675bf96ed2342aa33a55b3c8d7
sriniraghuna <sriniraghuna@gmail.com>
2025-05-17 10:19:49 -0500

    trigger all Chile config

--- scripts/trigger_all_chile_config.py
+++ scripts/trigger_all_chile_config.py
@@ -6,16 +6,23 @@ import numpy as np, os, sys
 cmd = 'python3 get_ilc_residuals.py -expname exppatchval -include_gal galval -which_gal_mask galmaskval -total_obs_time totalyearval -save_fg_res_and_weights 0 -final_comp cmb -interactive_mode 0'
 total_year_arr = np.arange(1, 10.1, 1)
 survey_arr = ['lat_wide', 'lat_wide_phase2', 'lat_wide_dc0', 'lat_delensing', 'lat_roman']
-survey_arr = ['lat_wide_phase2', 'lat_wide_dc0']
+#survey_arr = ['lat_wide_phase2', 'lat_wide_dc0']
+survey_arr = ['lat_wide', 'lat_wide_dc0']
 galval = 0
 galmaskval = 0
+combined_with_advanced_so_arr = [None]
 
-if (0): #with galaxy
+if (1): #with galaxy
     #survey_arr = ['lat_wide']
-    survey_arr = ['lat_wide_phase2', 'lat_wide_dc0']
+    #survey_arr = ['lat_wide_phase2', 'lat_wide_dc0']
+    survey_arr = ['lat_wide', 'lat_wide_dc0']
     galval = 1
     galmaskval = 2
 
+if (1): #Combined with advanced SO
+    combined_with_advanced_so_arr = ['advanced_so_baseline', 'advanced_so_goal']
+
+total = 0
 for survey in survey_arr:
     if survey in ['lat_wide', 'lat_delensing', 'lat_roman']:
         patch_arr = [1, 2, 3, 4]
@@ -26,11 +33,18 @@ for survey in survey_arr:
 
     for patch in patch_arr:
         for total_year in total_year_arr:
-            ###if total_year == 10.: continue
-            exppatchval = 's4_all_chile_config_%s---patch%s' %(survey, patch)
-            curr_cmd = cmd.replace('exppatchval', exppatchval).replace('galval', str(galval)).replace('galmaskval', str(galmaskval)).replace('totalyearval', str(total_year))
-            print('\n', curr_cmd)
-            os.system(curr_cmd)
+            for curr_advanced_so_config in combined_with_advanced_so_arr:
+                if curr_advanced_so_config is None:
+                    ###if total_year == 10.: continue
+                    exppatchval = 's4_all_chile_config_%s---patch%s' %(survey, patch)
+                else:
+                    exppatchval = 's4_all_chile_config_%s---patch%s+%s' %(survey, patch, curr_advanced_so_config)
+                curr_cmd = cmd.replace('exppatchval', exppatchval).replace('galval', str(galval)).replace('galmaskval', str(galmaskval)).replace('totalyearval', str(total_year))
+                print('\n', curr_cmd); ###sys.exit()
+                os.system(curr_cmd)
+                total += 1
+
+print('\nTotal = %s.\n' %(total))
 
 """
 if (1): #Advanced-SO
